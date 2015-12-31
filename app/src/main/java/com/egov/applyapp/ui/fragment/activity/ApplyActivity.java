15fad17e657707f7b348862b0ae4620889d7cd92
liujingyuan <906514731@qq.com>
2015-12-31 18:38:57 +0800

    commit take photos libs

--- app/src/main/java/com/egov/applyapp/ui/fragment/activity/ApplyActivity.java
+++ app/src/main/java/com/egov/applyapp/ui/fragment/activity/ApplyActivity.java
@@ -1,14 +1,18 @@
 package com.egov.applyapp.ui.fragment.activity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import com.egov.applyapp.R;
 import com.egov.applyapp.base.BaseActivity;
+import java.util.ArrayList;
+import me.crosswall.photo.pick.PickConfig;
 
 /**
  * 报销的界面
@@ -37,11 +41,29 @@ public class ApplyActivity extends BaseActivity {
         goBack(ApplyActivity.this, mToolbar);
 
     }
-    @OnClick(R.id.open_camer_bt) void openCam(){
-
-
+    @OnClick(R.id.open_camer_bt) void openPhotos(){
+        new PickConfig.Builder(this)
+                .pickMode(PickConfig.MODE_MULTIP_PICK)
+                .maxPickSize(30)
+                .spanCount(3)
+                //.showGif(true)
+                .checkImage(false) //default false
+                .useCursorLoader(false) //default true
+                .toolbarColor(R.color.colorPrimary)
+                .build();
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode!=RESULT_OK){
+            return;
+        }
 
+        if(requestCode==PickConfig.PICK_REQUEST_CODE){
+            ArrayList<String> pick = data.getStringArrayListExtra(PickConfig.EXTRA_STRING_ARRAYLIST);
+            Toast.makeText(this,"pick size:"+pick.size(),Toast.LENGTH_SHORT).show();
+        }
+    }
     @Override protected void initLocation() {
 
     }
---
+++ pickphotos/src/main/java/me/crosswall/photo/pick/views/PhotoView.java
@@ -0,0 +1,15 @@
+package me.crosswall.photo.pick.views;
+import java.util.List;
+import me.crosswall.photo.pick.model.PhotoDirectory;
+
+/**
+ * Created by yuweichen on 15/12/9.
+ */
+public interface PhotoView extends BaseView {
+    /**
+     * showPhotosView
+     * @param photoDirectories
+     */
+    void showPhotosView(List<PhotoDirectory> photoDirectories);
+
+}
